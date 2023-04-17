974341f58f4f8a294083c823a8105076619c60cf
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-04-17 16:32:50 +0100

    add final text and refactor

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -1,21 +1,18 @@
 class GrammarStats
-  def initialize(text)
-    @text = text
+  def initialize
     @passed_texts = 0
     @checked_texts = 0
   end
   
-  def check
-    fail "not valid sentence" if @text == ""
-    loop do
-      if (@text[0] == @text[0].capitalize) && ([".", "!", "?"].include? @text[-1])
-        @passed_texts += 1
-        @checked_texts += 1
-        return true
-      else
-        @checked_texts += 1
-        return false
-      end
+  def check(text)
+    # fail "not valid sentence" if text == ""
+    if (text[0] == text[0].capitalize) && ([".", "!", "?"].include? text[-1])
+      @passed_texts += 1
+      @checked_texts += 1
+      return true
+    else
+      @checked_texts += 1
+      return false
     end
   end
   
@@ -23,16 +20,15 @@ class GrammarStats
   # the check defined in the `check` method. The number 55 represents 55%.
   
   def percentage_good
-    puts @passed_texts
-    puts @checked_texts
-    @passed_texts / @checked_texts * 100
+    return (@passed_texts.to_f / @checked_texts.to_f * 100).round
   end
 end
 
-grammar_stats = GrammarStats.new("Valid string!")
-grammar_stats1 = GrammarStats.new("invalid string!")
-grammar_stats2 = GrammarStats.new("invalid string2?")
-grammar_stats.check
-grammar_stats1.check
-grammar_stats2.check
-grammar_stats.percentage_good
+# grammar_stats = GrammarStats.new
+# grammar_stats.check("Valid string!")
+# grammar_stats.check("invalid string!")
+# grammar_stats.check("invalid string2?")
+# grammar_stats.percentage_good
+
+
+
--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -4,32 +4,33 @@ describe GrammarStats do
   describe "#check" do
     context "given a text that starts with a capital letter and ends with an appropriate punctuation" do
       it "returns true" do
-        grammar_stats = GrammarStats.new("Hello we are Aubrey and Andrea.")
-        expect(grammar_stats.check()).to eq true
+        grammar_stats = GrammarStats.new
+        expect(grammar_stats.check("Hello we are Aubrey and Andrea.")).to eq true
       end
     end
 
     context "given a text that starts with a capital letter and ends without an appropriate punctuation mark" do
       it "returns false" do
-        grammar_stats = GrammarStats.new("Hello we are Aubrey and Andrea")
-        expect(grammar_stats.check).to eq false
-      end
-    end
-
-    context "given an empty string" do
-      it "fails" do
-        grammar_stats = GrammarStats.new("")
-        expect { grammar_stats.check }.to raise_error "not valid sentence"
+        grammar_stats = GrammarStats.new
+        expect(grammar_stats.check("Hello we are Aubrey and Andrea")).to eq false
       end
     end
   end
 
+    # context "given an empty string" do
+    #   it "fails" do
+    #     grammar_stats = GrammarStats.new
+    #     expect { grammar_stats.check("") }.to raise_error "not valid sentence"
+    #   end
+    # end
+
   describe "#percentage_good" do
     context "given three strings, one valid and two not" do
-      it "returns 50" do
-        grammar_stats = GrammarStats.new("Valid string!")
-        grammar_stats_1 = GrammarStats.new("invalid string!")
-        grammar_stats_2 = GrammarStats.new("invalid string2?")
+      it "returns 33" do
+        grammar_stats = GrammarStats.new
+        grammar_stats.check("Valid string!")
+        grammar_stats.check("invalid string!")
+        grammar_stats.check("invalid string2?")
         expect(grammar_stats.percentage_good).to eq 33
       end
     end
